6abe83cfc027c880da8cb4853e7180a9b656ec91
Joao Silva <joao-miguel.silva-da-silva@ext.ec.europa.eu>
2020-09-16 10:33:46 +0100

    added eu-north-1 to the list of all regions except ireland on the seclog create and update scripts

--- EC-Setup-SecLog.sh
+++ EC-Setup-SecLog.sh
@@ -56,7 +56,7 @@ export i=1
 export sp="/-\|"
 
 AWS_REGION='eu-west-1'
-ALL_REGIONS_EXCEPT_IRELAND='["ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-west-2","eu-west-3","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2"]'
+ALL_REGIONS_EXCEPT_IRELAND='["ap-northeast-1","ap-northeast-2","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1","eu-west-2","eu-west-3","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2"]'
 
 # parameters for scripts
 CFN_BUCKETS_TEMPLATE='CFN/EC-lz-s3-buckets.yml'
