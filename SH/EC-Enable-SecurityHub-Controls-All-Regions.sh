66316927afb9e65570006266466358aa29c670e3
Joao Silva <joao-miguel.silva-da-silva@ext.ec.europa.eu>
2021-02-23 10:04:43 +0100

    Fine tuning update scripts

--- SH/EC-Enable-SecurityHub-Controls-All-Regions.sh
+++ SH/EC-Enable-SecurityHub-Controls-All-Regions.sh
@@ -35,24 +35,22 @@ display_help() {
 configure() {
 
    
-
-    echo ""
-    echo "- Enable securityhub controls for all regions"
-    echo "-------------------------------------"
-    echo ""
-
     accountid=`aws --profile $PROFILE sts get-caller-identity --query 'Account' --output text`
 
+    echo ""
+    echo "- Enable securityhub controls for all regions. Account $accountid"
+    echo "-------------------------------------"
+    echo ""
 
     for region in $(aws --profile $PROFILE ec2 describe-regions --output text --query 'Regions[*].[RegionName]'); do
         echo "auto-enable-controls for securityhub for region $region ..."
         aws --profile $PROFILE --region $region securityhub batch-enable-standards --standards-subscription-requests StandardsArn="arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
         aws --profile $PROFILE --region $region securityhub batch-enable-standards --standards-subscription-requests StandardsArn="arn:aws:securityhub:$region::standards/aws-foundational-security-best-practices/v/1.0.0"
         aws --profile $PROFILE --region $region securityhub update-security-hub-configuration --auto-enable-controls
-
+        sleep 2
         # Disable "ControlId": "IAM.6", "Title": "Hardware MFA should be enabled for the root user"
         aws --profile $PROFILE --region $region securityhub update-standards-control --standards-control-arn "arn:aws:securityhub:$region:$accountid:control/aws-foundational-security-best-practices/v/1.0.0/IAM.6" --control-status "DISABLED" --disabled-reason "Managed by Cloud Broker Team"
-
+        sleep 2
         # Disable "ControlId": "CIS1.14", "1.14 Ensure hardware MFA is enabled for the \"root\" account"
         aws securityhub --profile $PROFILE --region $region update-standards-control --standards-control-arn "arn:aws:securityhub:$region:$accountid:control/cis-aws-foundations-benchmark/v/1.2.0/1.14" --control-status "DISABLED" --disabled-reason "Managed by Cloud Broker Team"
 
