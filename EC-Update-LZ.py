273f40466213dab289a32830052a26a5675d207f
EC2 Default User <ec2-user@ip-10-0-0-52.eu-west-1.compute.internal>
2021-05-20 13:59:17 +0000

    Revert "added logging and comments on functions"
    This reverts commit a0eb6813ed70194cae3f187f9803c85bde86d589.

--- EC-Update-LZ.py
+++ EC-Update-LZ.py
@@ -3,13 +3,10 @@
 import sys, getopt
 import subprocess, pkg_resources
 import os
-import logging
 import boto3
 import json
 
-logging.basicConfig(level=logging.DEBUG)
-
-account_id = ''
+accountId = ''
 
 def main(argv):
 
@@ -51,27 +48,24 @@ def display_help():
     print('python EC-Update-LZ.py -m <manifest> -s <seclogprofile> -o <orgprofile>')
 
 def get_account_id(force = False):
+    """Return string
+    
+    AccountId of the account defined in the profile
     """
-    This function gets te id of the account defined in the profile
-        :param force: flag to force the retrieval of the account ID 
-        :eturn: a string with the account id 
-    """
-    global account_id
+    global accountId
 
-    if account_id == '' or force == True:
+    if accountId == '' or force == True:
         client = boto3.client('sts')
         data = client.get_caller_identity()
-        account_id = data['Account']
-        
-    logging.debug("Account Id : {}".format(account_id))
-    return account_id
+        accountId = data['Account']
+    return accountId
 
 def get_linked_accounts():
+    """Return list
+
+    Linked accounts from a SECLOG account
     """
-    Function to retrieve the Linked accounts from a SECLOG account
-        :return: list with linked account details
-    """
-    linked_accounts = []
+    linked = []
     accountId = get_account_id()
     client = boto3.client('guardduty')
     data0 = client.list_detectors()
@@ -80,10 +74,9 @@ def get_linked_accounts():
         
         for member in data1['Members']:
             if member['RelationshipStatus'] == 'ENABLED':
-                linked_accounts.append(member['AccoundId'])
-                
-    logging.debug("Linked accounts : {}".format(linked_accounts))
-    return linked_accounts
+                linked.append(member['AccoundId'])
+    
+    return linked
 
     
 
