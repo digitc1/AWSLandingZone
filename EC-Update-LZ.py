d2efc4e149fd747d5c3d0842f009e3ca76dc1be1
Joao Silva <joao-miguel.silva-da-silva@ext.ec.europa.eu>
2021-06-30 17:03:43 +0200

    removed stack instance installation from stackset on missing regions

--- EC-Update-LZ.py
+++ EC-Update-LZ.py
@@ -45,7 +45,6 @@ stacksets = { 'SECLZ-Enable-Config-SecurityHub-Globally' :  { 'Template' : 'CFN/
 tags = []
 
 all_regions = ["ap-northeast-1","ap-northeast-2","ap-northeast-3","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1","eu-west-1", "eu-west-2","eu-west-3","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2"]
-all_regions_except_ireland = ["ap-northeast-1","ap-northeast-2","ap-northeast-3","ap-south-1","ap-southeast-1","ap-southeast-2","ca-central-1","eu-central-1","eu-north-1", "eu-west-2","eu-west-3","sa-east-1","us-east-1","us-east-2","us-west-1","us-west-2"]
 
 def main(argv):
     
@@ -1020,8 +1019,7 @@ def update_stackset(client, stackset, templates, params=[]):
                 Parameters=validate_params(params, template_body), 
                 Capabilities=capabilities,
                 OperationPreferences=operationPreferences,
-                Tags=apply_tags,
-                Regions=all_regions_except_ireland
+                Tags=apply_tags
                 )
             updated=False
         
